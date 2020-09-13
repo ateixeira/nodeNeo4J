1c18900ffd2c64ac63e494b7426d9b0494d6fd08
Andre Teixeira <andre.teixeira@jumbo.com>
2020-09-13 16:07:21 +0200

    :card_file_box: Create load seed data method.

--- src/db/migration.ts
+++ src/db/migration.ts
@@ -1,5 +1,7 @@
 import * as neo4j from 'neo4j-driver';
+import { QueryResult } from 'neo4j-driver';
 import driver from './index';
+import seedData from './seed.json';
 
 type DataNode = {
   name: string;
@@ -12,28 +14,78 @@ type DataTree = {
   data: DataNode[];
 };
 
+type LoadDataResult = {
+  success: boolean;
+  count: number;
+};
+
 class Migrations {
   private readonly session: neo4j.Session;
 
+  private readonly seedData: DataTree;
+
   constructor() {
     this.session = driver.session();
+    this.seedData = seedData;
   }
 
-  private loadSeedData = async (entry?: DataTree): Promise<void> => {
+  private buildCreationStatements() {
     const createNodeStatements: string[] = [];
     const createRelationshipsStatements: string[] = [];
     const newNodesList: string[] = [];
+
+    this.seedData.data.reverse().map(
+      async (node): Promise<void> => {
+        const nodeTag = `node${node.name.replace(/-/g, '')}`;
+        const nodeParent = `node${node.parent}`;
+        createNodeStatements.push(
+          `(${nodeTag}:Node {name: "${node.name}", description: "${node.description}"})`
+        );
+        if (node.parent) {
+          createRelationshipsStatements.push(
+            `(${nodeTag})-[:isChildOf]->(${nodeParent})`
+          );
+        }
+        newNodesList.push(nodeTag);
+      }
+    );
+    return [createNodeStatements, createRelationshipsStatements, newNodesList];
+  }
+
+  private async loadSeedData(entry?: DataTree): Promise<LoadDataResult> {
+    const [
+      createNodeStatements,
+      createRelationshipsStatements,
+      newNodesList
+    ] = this.buildCreationStatements();
+
     const session = driver.session();
+    let result;
+    let success = true;
     try {
-      const result = await session.run(
+      result = await this.session.run(
         `CREATE ${createNodeStatements} CREATE ${createRelationshipsStatements} WITH ${newNodesList} MATCH relations=()-[:isChildOf]-() RETURN relations`
       );
-      console.log('RESULT: ', result);
     } catch (e) {
-      console.log('ERROR: ', e);
+      success = false;
+      // eslint-disable-next-line no-console
+      console.error('ERROR: ', e);
     } finally {
       await session.close();
+      await driver.close();
     }
-    // await driver.close();
-  };
+
+    return Promise.resolve({
+      success,
+      count: result?.records.length || 0
+    });
+  }
+
+  public async run() {
+    const loadData = await this.loadSeedData();
+    console.info(loadData);
+  }
 }
+
+const migrations = new Migrations();
+migrations.run();
