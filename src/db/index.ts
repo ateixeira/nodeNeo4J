5c6f575249999aab6dc0c1886c6ebb0390433eb4
Andre Teixeira <andre.teixeira@jumbo.com>
2020-09-14 09:20:51 +0200

    :wrench: Config jest

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,14 @@
-import { auth, driver } from 'neo4j-driver';
+import neo4j, { auth, Driver, driver, Session } from 'neo4j-driver';
 
-/**
- * Neo4j driver configuration
- *
- * Return: configured neo4j driver.
- */
-export default driver(
-  'bolt://localhost:7687/',
-  auth.basic('neo4j', 'password')
-);
+export class Neo4j {
+  private static driver: Driver = driver(
+    'bolt://localhost:7687/',
+    auth.basic('neo4j', 'password')
+  );
+
+  private static session: Session = Neo4j.driver.session();
+
+  public static execute(request: string) {
+    return Neo4j.session.run(request);
+  }
+}
